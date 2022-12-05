9257a5560b28eaa93465faa91e6cf5d92397ac53
keallyma <ekeshantal@gmail.com>
2022-12-05 11:43:49 -0800

    pycorrections

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for 1st_element in range(len(my_list)):
-        print("{:d}".format(my_list[1st_element]))
+    for 1st_elem in range(len(my_list)):
+        print("{:d}".format(my_list[1st_elem]))
