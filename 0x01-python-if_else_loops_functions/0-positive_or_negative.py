5f28e85d88992d8f0bedb9677e06d47cad7b7c7f
keallyma <ekeshantal@gmail.com>
2022-12-06 12:37:46 -0800

    corrections

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,4 +6,4 @@ number = random.randint(-10, 10)
     elif number == 0:
         print(f"{number} is zero")
     else:
-        print (f"{number} is negative")
+        print(f"{number} is negative")
---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by HOlberton School."""
+
+    if a < b:
+        return (C)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
