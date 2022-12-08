4e0a7865fe5707e8ec0fd66d0d63015efc7d0c8c
keallyma <ekeshantal@gmail.com>
2022-12-08 06:22:53 -0800

    lest

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    '''A function that returns thequare value of all elements in a 
+    multi dimensional list being passsed as arguments.
+
+    Alllowed to use regular loops, map, etc
+    '''
+    new_1st = []
+    if len(matrix) == 0
+        return new_1st
+
+    new_1st = [[i*i for i in j] for j in matrix]
+    return new_1st
