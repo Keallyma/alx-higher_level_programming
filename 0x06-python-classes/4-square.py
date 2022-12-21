3024a91e3cd3d2ac60fb8ff500dba3b60d092084
keallyma <ekeshantal@gmail.com>
2022-12-21 13:12:13 -0800

    corect

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,6 +18,7 @@ class Square:
         '''Get/set the current size of the square.'''
         return (self.__size)
 
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,6 +26,7 @@ class Square:
         elif value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
 
         def area(self):
             '''Return the current area of the square.'''
