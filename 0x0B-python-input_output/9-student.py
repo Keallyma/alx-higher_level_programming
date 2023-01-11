0424181d7a6efe713481f80cbd7b55b2ba1593a3
keallyma <ekeshantal@gmail.com>
2023-01-10 17:12:59 -0800

    corr

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-
 class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
+        """Initialize a new Student.
         Args:
             first_name (str): The fisrt name of the student.
             last_name (str): The last name of the student.
